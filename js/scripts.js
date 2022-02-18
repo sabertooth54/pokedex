64d54f01d8c515a79f0aa226b19fa9eb129ff1db
sabertooth54 <mako95@hotmail.co.uk>
2022-02-18 09:46:31 +0100

    IIFE added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,48 @@
-
-var pokemonList = [{name:"Bulbasaur", type: ["grass","monster"],height: 0.7},{name:"Butterfree", type: "bug",height: 1.1},{name:"Metapod",type :"bug",height: 0.7},{name:"Charizard",type :["Dragon","Monster"], height: 1.7},{name:"Weedle",type :"bug", height: 0.3},{name:"Ekans",type: ["Dragon","Field"], height: 2.0},{name:"Arbok",type: ["Dragon","Field"], height: 3.5}]
-           
-            
-    pokemonList.forEach(element => console.log(element));
-
+    let pokemonRepository = (function () {
+  
+        let pokemonList = [
+          {
+            name:"Bulbasaur", 
+            type: ["grass","monster"],
+            height: 0.7},
+          {
+            name:"Butterfree", 
+            type: "bug",
+            height: 1.1},
+          {
+            name:"Metapod",
+            type :"bug",
+            height: 0.7},
+          {
+            name:"Charizard",
+            type :["Dragon","Monster"], 
+            height: 1.7},
+          {
+            name:"Weedle",
+            type :"bug", 
+            height: 0.3},
+          {
+            name:"Ekans",
+            type: ["Dragon","Field"], 
+            height: 2.0},
+          {
+            name:"Arbok",
+            type: ["Dragon","Field"], 
+            height: 3.5}];
+      
+        function getAll () {
+          return pokemonList;
+        }
+      
+        function add (pokemon) {
+          pokemonList.push (pokemon);
+        }
+        return {
+          getAll: getAll,
+          add: add
+          }
+          
+        }
+      )()
+      
+      console.log(pokemonRepository.getAll());
