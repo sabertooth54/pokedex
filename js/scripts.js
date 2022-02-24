1b67d82b3f3decd6611fa6158994fb4839d4a88c
sabertooth54 <mako95@hotmail.co.uk>
2022-02-24 23:36:34 +0100

    DOM Manipulation Practice added

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
     let pokemonRepository = (function () {
+      let repository = [
   
-        let pokemonList = [
           {
             name:"Bulbasaur", 
             type: ["grass","monster"],
@@ -30,19 +30,48 @@
             type: ["Dragon","Field"], 
             height: 3.5}];
       
-        function getAll () {
-          return pokemonList;
-        }
-      
-        function add (pokemon) {
-          pokemonList.push (pokemon);
-        }
-        return {
-          getAll: getAll,
-          add: add
-          }
+            function add(pokemon) {
+              if (
+                typeof pokemon === "object" &&
+                "name" in pokemon &&
+                "height" in pokemon &&
+                "types" in pokemon
+              ) {
+                repository.push(pokemon);
+              } else {
+                console.log("pokemon is not correct");
+              }
+            }
+            function getAll() {
+              return repository;
+            }
+            function addListItem(pokemon){
+              let pokemonList = document.querySelector(".pokemon-list");
+              let listpokemon = document.createElement("li");
+              let button = document.createElement("button");addEventListener("click", function() {console.log(pokemon)});
+              button.innerText = pokemon.name;
+              button.classList.add("button-class");
+              listpokemon.appendChild(button);
+              pokemonList.appendChild(listpokemon);
+            }
+
+            function showDetails(pokemon){
+              console.log(pokemonRepository);
+            }
           
-        }
-      )()
-      
-      console.log(pokemonRepository.getAll());
+          
+            return {
+              add: add,
+              getAll: getAll,
+              addListItem: addListItem
+              
+            };
+          })();
+          
+          pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+          
+          console.log(pokemonRepository.getAll());
+          
+          pokemonRepository.getAll().forEach(function (pokemon) {
+            pokemonRepository.addListItem(pokemon);
+          });
