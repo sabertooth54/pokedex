750a81b1a5d55a1592eb2464a291aea4912e847d
sabertooth54 <mako95@hotmail.co.uk>
2022-02-28 15:30:22 +0100

    API added

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,83 @@
     let pokemonRepository = (function () {
-      let repository = [
+      let repository = []; 
+      let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';  
   
-          {
-            name:"Bulbasaur", 
-            type: ["grass","monster"],
-            height: 0.7},
-          {
-            name:"Butterfree", 
-            type: "bug",
-            height: 1.1},
-          {
-            name:"Metapod",
-            type :"bug",
-            height: 0.7},
-          {
-            name:"Charizard",
-            type :["Dragon","Monster"], 
-            height: 1.7},
-          {
-            name:"Weedle",
-            type :"bug", 
-            height: 0.3},
-          {
-            name:"Ekans",
-            type: ["Dragon","Field"], 
-            height: 2.0},
-          {
-            name:"Arbok",
-            type: ["Dragon","Field"], 
-            height: 3.5}];
+         
+      function add(pokemon) {
+        if (
+          typeof pokemon === "object" &&
+          "name" in pokemon
+        ) {
+          repository.push(pokemon);
+        } else {
+          console.log("pokemon is not correct");
+        }
+      }
+      function getAll() {
+        return repository;
+      }
+      function addListItem(pokemon){
+        let repository = document.querySelector(".pokemon-list");
+        let listpokemon = document.createElement("li");
+        //In your addListItem() function, add an event listener to the button you created. It should listen to a click. As for its event handler function, call the showDetails function there, passing the pokemon object as a parameter when a Pokémon is clicked.//
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        repository.appendChild(listpokemon);
+        button.addEventListener("click", () =>{
+          showDetails(pokemon);
+        });
+      }   
       
-            function add(pokemon) {
-              if (
-                typeof pokemon === "object" &&
-                "name" in pokemon &&
-                "height" in pokemon &&
-                "types" in pokemon
-              ) {
-                repository.push(pokemon);
-              } else {
-                console.log("pokemon is not correct");
-              }
-            }
-            function getAll() {
-              return repository;
-            }
-            function addListItem(pokemon){
-              let pokemonList = document.querySelector(".pokemon-list");
-              let listpokemon = document.createElement("li");
-              //In your addListItem() function, add an event listener to the button you created. It should listen to a click. As for its event handler function, call the showDetails function there, passing the pokemon object as a parameter when a Pokémon is clicked.//
-              let button = document.createElement("button");
-              button.innerText = pokemon.name;
-              button.classList.add("button-class");
-              listpokemon.appendChild(button);
-              pokemonList.appendChild(listpokemon);
-              button.addEventListener("click", () =>{
-                showDetails(pokemon);
+      function loadList() {
+        return fetch(apiUrl).then(function (response) {
+          return response.json();
+        }).then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+          });
+        }).catch(function (e) {
+          console.error(e);
+        })
+      }  
+      
+      
+              function loadDetails(item) {
+              let url = item.detailsUrl;
+              return fetch(url).then(function (response) {
+                return response.json();
+              }).then(function (details) {
+                item.imageUrl = details.sprites.front_default;
+                item.height = details.height;
+                item.types =details.types;
+              }).catch(function (e) {
+                console.error(e);
               });
             }
-
-            function showDetails(pokemon){
-              console.log(pokemon);
-            }
-          
-          
+            function showDetails(pokemon) {
+              pokemonRepository.loadDetails(pokemon).then(function () {
+                console.log(pokemon);
+            });
+          }        
             return {
               add: add,
               getAll: getAll,
-              addListItem: addListItem
-              
+              addListItem: addListItem,
+              loadList: loadList,
+              loadDetails: loadDetails,
+              showDetails: showDetails
             };
-          })();
+          })();  
           
-          pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
-          
-          console.log(pokemonRepository.getAll());
-          
-          pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.loadList().then(function () {  
+          pokemonRepository.getAll().forEach(function (pokemon) {
             pokemonRepository.addListItem(pokemon);
-          }
-        );
+          });
+        });
+
+       
